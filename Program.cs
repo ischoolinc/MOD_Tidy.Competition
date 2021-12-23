b93e1364a9cb29842e098a68e3e6e15c2ef6c3d5
Cynthia Chen <74887146+cynthiaischool@users.noreply.github.com>
2021-12-23 14:29:08 +0800

    2021-12-15 Cynthia 因原先操作指南無法在x64版本上作為背景載入，故參考俊威的意見，將操作指南另外用一個button點擊開啟。
    https://3.basecamp.com/4399967/buckets/15765350/todos/4448312891

--- Program.cs
+++ Program.cs
@@ -110,7 +110,19 @@ namespace Ischool.Tidy_Competition
             #region 整潔競賽
             {
                 // 整潔競賽模組頁面
-                MotherForm.AddPanel(TidyCompetitionPanel.Instance);
+
+                //2021-12-15 Cynthia 因原先操作指南無法在x64版本上作為背景載入，故參考俊威的意見，將操作指南另外用一個button點擊開啟。
+                //MotherForm.AddPanel(TidyCompetitionPanel.Instance);
+                MotherForm.RibbonBarItems["整潔競賽", "使用說明"]["操作指南"].Image = Properties.Resources._03;
+                MotherForm.RibbonBarItems["整潔競賽", "使用說明"]["操作指南"].Size = RibbonBarButton.MenuButtonSize.Large;
+                #region 操作指南
+                {
+                    MotherForm.RibbonBarItems["整潔競賽", "使用說明"]["操作指南"].Click += delegate
+                    {
+                        System.Diagnostics.Process.Start("https://sites.google.com/ischool.com.tw/neat-competition/%E9%A6%96%E9%A0%81");
+                    };
+                }
+                #endregion
 
                 MotherForm.RibbonBarItems["整潔競賽", "基本設定"]["人員設定"].Image = Properties.Resources.foreign_language_config_64;
                 MotherForm.RibbonBarItems["整潔競賽", "基本設定"]["人員設定"].Size = RibbonBarButton.MenuButtonSize.Large;
